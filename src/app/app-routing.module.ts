d313013516e590b16ee11cca1068faa1299e76f6
john <chukwuchidieberejohn@gmail.com>
2023-04-22 17:28:57 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+import {HomePageComponent} from "./components/home-page/home-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "home", component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from "@angular/core";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+
+@Component({
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.css"]
+})
+export class HomePageComponent implements OnInit {
+
+  public safeURL: SafeResourceUrl;
+
+  constructor(private _sanitizer: DomSanitizer) {
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/o_joulYVndM");
+    // this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl("./assets/TypeScript Tutorial for Beginners.mp4");
+  }
+
+  ngOnInit(): void {
+  }
+
+}
